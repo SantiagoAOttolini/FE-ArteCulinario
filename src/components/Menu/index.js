7b5c110665c112b0f56204db0b411647363cb80b
santiago <santiago.ottolini98@gmail.com>
2021-01-11 12:49:41 -0300

    AC-FE: Change start menu

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,116 +1,34 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import "./style.css";
-
+import {
+  Navbar,
+  Nav,
+} from "react-bootstrap";
+import "./style.css"
 class Menu extends Component {
   render() {
     return (
       <div className="containerMenu">
-        <div className="header">
-          <div className="title">
-            <h1 className="text1">Arte Culinario</h1>
-            <h1 className="text2">Bienvenidos</h1>
-          </div>
-          <div className="nav">
-            <ul>
-              <li>
-                <Link className="login" to="/Login">
-                  Login
-                </Link>
-              </li>
-              <li>
-                {" "}
-                <Link className="foodCompare" to="/compare">
-                  Comparar alimentos
-                </Link>
-              </li>
-              <li>
-                {" "}
-                <Link className="beneficios" to="/beneficios">
-                  Beneficios
-                </Link>
-              </li>
-              <li>
-                {" "}
-                <Link className="register" to="/registro">
-                  Registro
-                </Link>
-              </li>
-              <li>
-                {" "}
-                <Link className="controlpeso" to="/controlpeso">
-                  Control de peso
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="wrapper">
-          <div className="section">
-            <h2>Introduccion</h2>
-            <p>
-              Arte Culinario es un sitio web relacionado con la aplicación móvil
-              para sistemas operativo android Cocin-Arte. Las personas podrán
-              utilizar el sitio para comparar entre dos alimentos y acceder a la
-              información nutricional de los mismos para validar sus diferencias
-              y ver que beneficios tiene cada una para llevar una vida
-              saludable. Además permitirá a los usuarios que ya dispongan de una
-              la versión premium de la aplicación móvil u otros que quieran
-              acceder a la versión premium a través del sitio web , acceso a
-              códigos promocionales de descuentos en locales de gastronomía de
-              su localidad fomentando al consumo.
-            </p>
-            <p>
-              Arte Culinario es un sitio web relacionado con la aplicación móvil
-              para sistemas operativo android Cocin-Arte. Las personas podrán
-              utilizar el sitio para comparar entre dos alimentos y acceder a la
-              información nutricional de los mismos para validar sus diferencias
-              y ver que beneficios tiene cada una para llevar una vida
-              saludable. Además permitirá a los usuarios que ya dispongan de una
-              la versión premium de la aplicación móvil u otros que quieran
-              acceder a la versión premium a través del sitio web , acceso a
-              códigos promocionales de descuentos en locales de gastronomía de
-              su localidad fomentando al consumo.
-            </p>
-            <p>
-              Arte Culinario es un sitio web relacionado con la aplicación móvil
-              para sistemas operativo android Cocin-Arte. Las personas podrán
-              utilizar el sitio para comparar entre dos alimentos y acceder a la
-              información nutricional de los mismos para validar sus diferencias
-              y ver que beneficios tiene cada una para llevar una vida
-              saludable. Además permitirá a los usuarios que ya dispongan de una
-              la versión premium de la aplicación móvil u otros que quieran
-              acceder a la versión premium a través del sitio web , acceso a
-              códigos promocionales de descuentos en locales de gastronomía de
-              su localidad fomentando al consumo.
-            </p>
-            <p>
-              Arte Culinario es un sitio web relacionado con la aplicación móvil
-              para sistemas operativo android Cocin-Arte. Las personas podrán
-              utilizar el sitio para comparar entre dos alimentos y acceder a la
-              información nutricional de los mismos para validar sus diferencias
-              y ver que beneficios tiene cada una para llevar una vida
-              saludable. Además permitirá a los usuarios que ya dispongan de una
-              la versión premium de la aplicación móvil u otros que quieran
-              acceder a la versión premium a través del sitio web , acceso a
-              códigos promocionales de descuentos en locales de gastronomía de
-              su localidad fomentando al consumo.
-            </p>
-            <p>
-              Arte Culinario es un sitio web relacionado con la aplicación móvil
-              para sistemas operativo android Cocin-Arte. Las personas podrán
-              utilizar el sitio para comparar entre dos alimentos y acceder a la
-              información nutricional de los mismos para validar sus diferencias
-              y ver que beneficios tiene cada una para llevar una vida
-              saludable. Además permitirá a los usuarios que ya dispongan de una
-              la versión premium de la aplicación móvil u otros que quieran
-              acceder a la versión premium a través del sitio web , acceso a
-              códigos promocionales de descuentos en locales de gastronomía de
-              su localidad fomentando al consumo.
-            </p>
-            <p>Colaboracion con Cocin-Arte</p>
-          </div>
-        </div>
+          {/* <div className="d-flex justify-content-center"><h1 className="title">Bienvenidos</h1></div> */}
+        <Navbar fixed="top"collapseOnSelect expand="lg" bg="light" >
+          <Navbar.Brand href="#home">Arte Culinario</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#foodCompare">Comparar alimentos</Nav.Link>
+              <Nav.Link href="#weightControl">Control de peso</Nav.Link>
+              <Nav.Link href="#metabolicAnalyzer">Analizador metabolico</Nav.Link>
+              <Nav.Link href="#benefit">Beneficios</Nav.Link>
+
+            </Nav>
+            <Nav>
+              <Nav.Link href="#login">Login</Nav.Link>
+              <Nav.Link eventKey={2} href="#register">
+                Registro
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
       </div>
     );
   }
