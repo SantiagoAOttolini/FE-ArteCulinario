9cf857812fd19de7536d64d36b3c7ddc36f57fbc
santiago <santiago.ottolini98@gmail.com>
2021-01-11 13:07:59 -0300

    AC-FE: Change Href for Link

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -5,27 +5,30 @@ import {
   Nav,
 } from "react-bootstrap";
 import "./style.css"
+
 class Menu extends Component {
+    
   render() {
     return (
       <div className="containerMenu">
+          
           {/* <div className="d-flex justify-content-center"><h1 className="title">Bienvenidos</h1></div> */}
         <Navbar fixed="top"collapseOnSelect expand="lg" bg="light" >
           <Navbar.Brand href="#home">Arte Culinario</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mr-auto">
-              <Nav.Link href="#foodCompare">Comparar alimentos</Nav.Link>
-              <Nav.Link href="#weightControl">Control de peso</Nav.Link>
-              <Nav.Link href="#metabolicAnalyzer">Analizador metabolico</Nav.Link>
-              <Nav.Link href="#benefit">Beneficios</Nav.Link>
+              <Link className="text-dark mr-3" to="/foodCompare">Comparar alimentos</Link>
+              <Link className="text-dark mr-3" to="/weightControl">Control de peso</Link>
+              <Link className="text-dark mr-3" to="/metabolicAnalyzer">Analizador metabolico</Link>
+              <Link className="text-dark" to="/benefit">Beneficios</Link>
 
             </Nav>
             <Nav>
-              <Nav.Link href="#login">Login</Nav.Link>
-              <Nav.Link eventKey={2} href="#register">
+              <Link className="text-dark mr-3" to="/login">Login</Link>
+              <Link className="text-dark" eventKey={2} to="/register">
                 Registro
-              </Nav.Link>
+              </Link>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
