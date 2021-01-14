4bd938d6888f1d17f7a5a8f48c9121e2ed361adf
Facundo Loberse <facundosa123@gmail.com>
2021-01-13 23:18:34 -0300

    AC-FE: Added metabolic analyzer

--- src/container/App/index.js
+++ src/container/App/index.js
@@ -1,16 +1,18 @@
-import React, { Suspense } from "react";
-import { Route, Switch } from "react-router-dom";
-import Auth from "../../hoc/auth";
+import React, { Suspense } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Auth from '../../hoc/auth'
 // pages for this product
-import LandingPage from "../../components/views/LandingPage/LandingPage.js";
-import LoginPage from "../../components/views/Security Model/LoginPage/LoginPage";
-import RegisterPage from "../../components/views/Security Model/RegisterPage/RegisterPage";
-import NavBar from "../../components/views/NavBar/NavBar";
-import forgotPassword from "../../components/views/Security Model/LoginPage/ForgotPassword";
-import changePassword from "../../components/views/Security Model/LoginPage/ResetPassword";
-import weightControl from "../../components/views/Weight Control/Weight"
-import foodCompare from "../../components/views/Food Compare/Home"
-import analyzerMetabolic from "../../components/views/Analyzer/Home/home"
+import LandingPage from '../../components/views/LandingPage/LandingPage.js'
+import LoginPage from '../../components/views/Security Model/LoginPage/LoginPage'
+import RegisterPage from '../../components/views/Security Model/RegisterPage/RegisterPage'
+import NavBar from '../../components/views/NavBar/NavBar'
+import forgotPassword from '../../components/views/Security Model/LoginPage/ForgotPassword'
+import changePassword from '../../components/views/Security Model/LoginPage/ResetPassword'
+import weightControl from '../../components/views/Weight Control/Weight'
+import foodCompare from '../../components/views/Food Compare/Home'
+import analyzerMetabolic from '../../components/views/Analyzer/Home/home'
+import bulking from '../../components/views/Analyzer/Bulking-Cutting/bulking'
+import cutting from '../../components/views/Analyzer/Bulking-Cutting/cutting'
 
 //null   Anyone Can go inside
 //true   only logged in user can go inside
@@ -20,29 +22,38 @@ function App() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <NavBar />
-      <div style={{ paddingTop: "69px", minHeight: "calc(100vh - 80px)" }}>
+      <div style={{ paddingTop: '69px', minHeight: 'calc(100vh - 80px)' }}>
         <Switch>
-          
-          <Route exact path="/" component={Auth(LandingPage, null)} />
-          <Route exact path="/login" component={Auth(LoginPage, false)} />
-          <Route exact path="/register" component={Auth(RegisterPage, false)} />
-          <Route exact path="/foodCompare" component={Auth(foodCompare, null)} />
+          <Route exact path='/' component={Auth(LandingPage, null)} />
+          <Route exact path='/login' component={Auth(LoginPage, false)} />
+          <Route exact path='/register' component={Auth(RegisterPage, false)} />
           <Route
             exact
-            path="/forgotPassword"
+            path='/foodCompare'
+            component={Auth(foodCompare, null)}
+          />
+          <Route
+            exact
+            path='/forgotPassword'
             component={Auth(forgotPassword, false)}
           />
           <Route
             exact
-            path="/changePassword"
+            path='/changePassword'
             component={Auth(changePassword, true)}
           />
-          <Route exact path="/weightControl" component={weightControl} />
-          <Route exact path="/analyzerMetabolic" component={analyzerMetabolic} />
+          <Route exact path='/weightControl' component={weightControl} />
+          <Route
+            exact
+            path='/analyzerMetabolic'
+            component={analyzerMetabolic}
+          />
+          <Route exact path='/bulking' component={Auth(bulking, null)} />
+          <Route exact path='/cutting' component={Auth(cutting, null)} />
         </Switch>
       </div>
     </Suspense>
-  );
+  )
 }
 
-export default App;
+export default App
